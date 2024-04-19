624c5584eddde20bbbed8dc5dadcaabab2705d1c
tobeprozy <41546485+tobeprozy@users.noreply.github.com>
2024-04-19 16:59:48 +0800

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -7,4 +7,23 @@ echo "Requirements installed successfully."
 python3 -m dfss --url=open@sophgo.com:sophon-demo/Whisper/model_240327/models.zip
 unzip models.zip
 rm models.zip
+
+
+mkdir -p models/BM1684X
+pushd ../models/BM1684X
+python3 -m dfss --url=open@sophgo.com:sophon-demo/Qwen/qwen-7b_int4_1dev.bmodel
+python3 -m dfss --url=open@sophgo.com:sophon-demo/Qwen/qwen-7b_int8_1dev.bmodel
+popd
+
+if [ ! -d "/token_config" ];
+then
+    pushd .
+    python3 -m dfss --url=open@sophgo.com:sophon-demo/Qwen/token_config.zip
+    unzip token_config.zip
+    rm token_config.zip
+    popd
+else
+    echo "token_config exists! Remove it if you need to update."
+fi
+
 echo "Models download successfully."
